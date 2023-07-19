f8cae364a928cfb47ce6dd289eb7b3213287dd44
MagikBased <MagikBased@disroot.org>
2023-07-19 00:09:31 -0500

    Finished Board Setup

--- Scripts/BoardSprite.gd
+++ Scripts/BoardSprite.gd
@@ -1,16 +1,35 @@
 extends Sprite2D
 
+enum PieceType{
+	King,
+	Pawn,
+	Lance,
+	Knight,
+	Silver,
+	Gold,
+	Bishop,
+	Rook
+}
+enum Player{
+	Sente,
+	Gote
+}
+
 @export var boardSize = Vector2(9, 9)
 var lineSize = 8 #should be divisible by 4 for even lines
 var squareSize = (texture.get_width()) / boardSize.x
-var gridColor = Color(0,0,0) # Change this to set the color of the grid lines
+var gridColor = Color(0,0,0)
 var circleRadius = squareSize * 0.15
 var circleColor = Color(0,0,0)
-#var font = Font
-#signal boardScale
+#var font: Font = get_font("font")
 
 func _ready():
-	pass
+	board_setup()
+
+func find_square_center(file: int,rank: int) -> Vector2:
+	var centerX = (10 - file) * squareSize - squareSize / 2
+	var centerY = rank * squareSize - squareSize / 2
+	return Vector2(centerX, centerY)
 
 func draw_grid():
 	for x in range(1, boardSize.x):
@@ -25,7 +44,96 @@ func draw_grid():
 	draw_circle(Vector2(squareSize*6,squareSize*3),circleRadius, circleColor)
 	draw_circle(Vector2(squareSize*6,squareSize*6),circleRadius, circleColor)
 	
-	#draw_string()
+	#draw_circle(find_square_center(9,2),circleRadius,circleColor)
+	#print(str(find_square_center(9,2))+ " local find square cetner of 9,2")
+	#draw_circle(Vector2(0,0),40,Color.RED)
+	#draw_string(font, find_square_center(0,1),"1",HORIZONTAL_ALIGNMENT_CENTER,10,12,Color.BLACK,TextServer.JUSTIFICATION_NONE,TextServer.DIRECTION_AUTO,TextServer.ORIENTATION_HORIZONTAL)
 	
 func _draw():
 	draw_grid()
+
+
+func board_setup():
+#	var piecePositions = {
+#		"Lance1": Vector2(1, 9),
+#		"Knight1": Vector2(2, 9),
+#		"Silver1": Vector2(3, 9),
+#		"Gold1": Vector2(4, 9),
+#		"King": Vector2(5, 9),
+#		"Gold2": Vector2(6, 9),
+#		"Silver2": Vector2(7, 9),
+#		"Knight2": Vector2(8, 9),
+#		"Lance2": Vector2(9, 9),
+#		"Rook1": Vector2(2, 8),
+#		"Bishop1": Vector2(8, 8),
+#		"Pawn1": Vector2(1, 7),
+#		"Pawn2": Vector2(2, 7),
+#		"Pawn3": Vector2(3, 7),
+#		"Pawn4": Vector2(4, 7),
+#		"Pawn5": Vector2(5, 7),
+#		"Pawn6": Vector2(6, 7),
+#		"Pawn7": Vector2(7, 7),
+#		"Pawn8": Vector2(8, 7),
+#		"Pawn9": Vector2(9, 7),
+#		"Rook2": Vector2(2, 1),
+#		"Bishop2": Vector2(8, 1),
+#		"Pawn10": Vector2(1, 3),
+#		"Pawn11": Vector2(2, 3),
+#		"Pawn12": Vector2(3, 3),
+#		"Pawn13": Vector2(4, 3),
+#		"Pawn14": Vector2(5, 3),
+#		"Pawn15": Vector2(6, 3),
+#		"Pawn16": Vector2(7, 3),
+#		"Pawn17": Vector2(8, 3),
+#		"Pawn18": Vector2(9, 3),
+#	}
+#	for piece_name in piecePositions.keys():
+	create_piece(PieceType.Lance, Player.Sente, Vector2(1,9))
+	create_piece(PieceType.Knight, Player.Sente, Vector2(2,9))
+	create_piece(PieceType.Silver, Player.Sente, Vector2(3,9))
+	create_piece(PieceType.Gold, Player.Sente, Vector2(4,9))
+	create_piece(PieceType.King, Player.Sente, Vector2(5,9))
+	create_piece(PieceType.Gold, Player.Sente, Vector2(6,9))
+	create_piece(PieceType.Silver, Player.Sente, Vector2(7,9))
+	create_piece(PieceType.Knight, Player.Sente, Vector2(8,9))
+	create_piece(PieceType.Lance, Player.Sente, Vector2(9,9))
+	create_piece(PieceType.Bishop, Player.Sente, Vector2(2,8))
+	create_piece(PieceType.Rook, Player.Sente, Vector2(8,8))
+	create_piece(PieceType.Pawn, Player.Sente, Vector2(1,7))
+	create_piece(PieceType.Pawn, Player.Sente, Vector2(2,7))
+	create_piece(PieceType.Pawn, Player.Sente, Vector2(3,7))
+	create_piece(PieceType.Pawn, Player.Sente, Vector2(4,7))
+	create_piece(PieceType.Pawn, Player.Sente, Vector2(5,7))
+	create_piece(PieceType.Pawn, Player.Sente, Vector2(6,7))
+	create_piece(PieceType.Pawn, Player.Sente, Vector2(7,7))
+	create_piece(PieceType.Pawn, Player.Sente, Vector2(8,7))
+	create_piece(PieceType.Pawn, Player.Sente, Vector2(9,7))
+	
+	create_piece(PieceType.Lance, Player.Gote, Vector2(1,1))
+	create_piece(PieceType.Knight, Player.Gote, Vector2(2,1))
+	create_piece(PieceType.Silver, Player.Gote, Vector2(3,1))
+	create_piece(PieceType.Gold, Player.Gote, Vector2(4,1))
+	create_piece(PieceType.King, Player.Gote, Vector2(5,1))
+	create_piece(PieceType.Gold, Player.Gote, Vector2(6,1))
+	create_piece(PieceType.Silver, Player.Gote, Vector2(7,1))
+	create_piece(PieceType.Knight, Player.Gote, Vector2(8,1))
+	create_piece(PieceType.Lance, Player.Gote, Vector2(9,1))
+	create_piece(PieceType.Bishop, Player.Gote, Vector2(2,2))
+	create_piece(PieceType.Rook, Player.Gote, Vector2(8,2))
+	create_piece(PieceType.Pawn, Player.Gote, Vector2(1,3))
+	create_piece(PieceType.Pawn, Player.Gote, Vector2(2,3))
+	create_piece(PieceType.Pawn, Player.Gote, Vector2(3,3))
+	create_piece(PieceType.Pawn, Player.Gote, Vector2(4,3))
+	create_piece(PieceType.Pawn, Player.Gote, Vector2(5,3))
+	create_piece(PieceType.Pawn, Player.Gote, Vector2(6,3))
+	create_piece(PieceType.Pawn, Player.Gote, Vector2(7,3))
+	create_piece(PieceType.Pawn, Player.Gote, Vector2(8,3))
+	create_piece(PieceType.Pawn, Player.Gote, Vector2(9,3))
+
+func create_piece(piece_name,piece_owner,starting_position):
+	var piece_scene = load("res://Scenes/piece.tscn")
+	var piece = piece_scene.instantiate()
+	piece.pieceType = piece_name
+	piece.pieceOwner = piece_owner
+	piece.currentPosition = starting_position
+	get_parent().add_child.call_deferred(piece)
--- Scripts/piece.gd
+++ Scripts/piece.gd
@@ -15,21 +15,58 @@ enum Player{
 	Gote
 }
 
-#var boardScale = $Board.BoardSprite.squareSize
-var globalPieceScale = 1
+@onready var board = get_parent()
+@onready var boardSprite = board.get_node("BoardSprite")
+@onready var globalPieceScale = (boardSprite.texture.get_width() * boardSprite.scale.x) / (boardSprite.boardSize.x * texture.get_width())
+@onready var boardPosition = board.global_position
 
 @export var pieceType = PieceType.Pawn
 @export var pieceOwner = Player.Sente
 @export var promoted: bool = false
 
+@export var currentPosition: Vector2 = Vector2(1,9)
 var selected: bool = false
+var dragging: bool = false
+var dragging_position: Vector2
 
 func _ready():
 	scale *= globalPieceScale
 	set_piece_type()
+	snap_to_grid()
 	if pieceOwner == Player.Gote:
 		rotation_degrees += 180
-		
+
+func snap_to_grid():
+	var posNotation:Vector2 = boardSprite.find_square_center(currentPosition.x,currentPosition.y)
+	position = posNotation * boardSprite.scale
+	#print(posNotation)
+	#print(to_global(posNotation))
+	#print("piece position " + str(position))
+
 func set_piece_type():
+	var sprite_texture = null
+	match pieceType:
+		PieceType.King:
+			sprite_texture = load("res://Images/Pieces/King.png")
+		PieceType.Pawn:
+			sprite_texture = load("res://Images/Pieces/Pawn.png")
+		PieceType.Lance:
+			sprite_texture = load("res://Images/Pieces/Lance.png")
+		PieceType.Knight:
+			sprite_texture = load("res://Images/Pieces/Knight.png")
+		PieceType.Silver:
+			sprite_texture = load("res://Images/Pieces/Silver General.png")
+		PieceType.Gold:
+			sprite_texture = load("res://Images/Pieces/Gold General.png")
+		PieceType.Bishop:
+			sprite_texture = load("res://Images/Pieces/Bishop.png")
+		PieceType.Rook:
+			sprite_texture = load("res://Images/Pieces/Rook.png")
+		_:
+			sprite_texture = null
+	texture = sprite_texture
+
+func _input(_event):
 	pass
+
 
